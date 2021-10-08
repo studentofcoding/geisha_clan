140e13dda363913fb80d7425808908d00ed641ca
Yonathan Evan C <yonathanevan@gmail.com>
2021-10-08 09:00:40 +0700

    Update placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "./App.css";
+import "./adds_a.css";
+import "./adds_b.css";
+
 import { useMemo } from "react";
+import Header from './components/Header';
 
 import Home from "./Home";
 
@@ -21,6 +25,9 @@ import {
 
 import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
 import { createTheme, ThemeProvider } from "@material-ui/core";
+
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 const treasury = new anchor.web3.PublicKey(
   process.env.REACT_APP_TREASURY_ADDRESS!
@@ -46,78 +53,171 @@ const txTimeout = 30000; // milliseconds (confirm this works for your project)
 const nftName = "Geisha Clan";
 
 const theme = createTheme({
-    palette: {
-        type: 'dark',
+  palette: {
+    type: "dark",
+  },
+  overrides: {
+    MuiButtonBase: {
+      root: {
+        justifyContent: "flex-start",
+      },
     },
-    overrides: {
-        MuiButtonBase: {
-            root: {
-                justifyContent: 'flex-start',
-            },
-        },
-        MuiButton: {
-            root: {
-                textTransform: undefined,
-                padding: '12px 16px',
-            },
-            startIcon: {
-                marginRight: 8,
-            },
-            endIcon: {
-                marginLeft: 8,
-            },
-        },
+    MuiButton: {
+      root: {
+        textTransform: undefined,
+        padding: "12px 16px",
+      },
+      startIcon: {
+        marginRight: 8,
+      },
+      endIcon: {
+        marginLeft: 8,
+      },
     },
+  },
 });
+
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+  <img src="./showcase-1.png" onDragStart={handleDragStart} role="presentation" />,
+  <img src="./showcase-2.png" onDragStart={handleDragStart} role="presentation" />,
+  <img src="./showcase-3.png" onDragStart={handleDragStart} role="presentation" />,
+  <img src="./showcase-4.png" onDragStart={handleDragStart} role="presentation" />,
+  <img src="./showcase-5.png" onDragStart={handleDragStart} role="presentation" />
+];
 
 const App = () => {
   const endpoint = useMemo(() => clusterApiUrl(network), []);
 
   const wallets = useMemo(
     () => [
-        getPhantomWallet(),
-        getSlopeWallet(),
-        getSolflareWallet(),
-        getSolletWallet({ network }),
-        getSolletExtensionWallet({ network })
+      getPhantomWallet(),
+      getSlopeWallet(),
+      getSolflareWallet(),
+      getSolletWallet({ network }),
+      getSolletExtensionWallet({ network }),
     ],
     []
   );
 
   return (
-      <ThemeProvider theme={theme}>
-        <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect={true}>
-            <WalletDialogProvider>
-              <div className='app-container'>
-                <section className='section main-section'>
-                  <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
-                    {/* Left Col */}
-                    <div className="w-full md:w-3/5 justify-center text-center">
-                      <img className="md:w-5/5 z-50" width="550px" alt="NFT name" src="/logo.png" />
-                    </div>
-                    {/* Right Col */}
-                    <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-                      <p className='title text-2xl font-bold'>This will be <span className='font-bold'>{nftName}</span> headline</p>
-                      <p className='mt-3'>
-                        This will be {nftName} short description
-                      </p>
-                      <Home
-                        candyMachineId={candyMachineId}
-                        config={config}
-                        connection={connection}
-                        startDate={startDateSeed}
-                        treasury={treasury}
-                        txTimeout={txTimeout}
-                      />
-                    </div>
+    <ThemeProvider theme={theme}>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect={true}>
+          <WalletDialogProvider>
+            <div className="app-container">
+              <Header />
+              <section id="home" className="Hero_Wrapper__3gskU">
+                <div className="Container Hero_Container__alybC">
+                  {/* <img
+                    className="Hero_Bookmark__1UTaa"
+                    src="./img-bookmark-icon.svg"
+                    alt="Bookmark Icon"
+                    draggable="false"
+                  /> */}
+                  <img
+                    className="Hero_TextLogo__3d9mh"
+                    src="./img-hero-text.svg"
+                    alt="Sneaky Vampire Syndicate"
+                    draggable="false"
+                  />
+                  {/* <h1 className="title_home">
+                    THE GEISHA CLAN
+                  </h1> */}
+                  <div className="Hero_LinksContainer__De3u8">
+                    <a
+                      href="javascript:void(0)"
+                      rel="noreferrer"
+                    >
+                      Discord
+                    </a>
+                    <a
+                      href="javascript:void(0)"
+                      rel="noreferrer"
+                    >
+                      Twitter
+                    </a>
                   </div>
-                </section>
-              </div>
-            </WalletDialogProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      </ThemeProvider>
+                </div>
+              </section>
+              <section id="journey" className="section">
+                <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
+                  <div className="flex flex-col w-full justify-center items-start text-center">
+                    <p className="title text-xl sm:text-5xl font-bold w-full">
+                      NEW JOURNEY
+                    </p>
+                    <p className="content mt-3">
+                      AFTER TAKING PART IN A HUGE HISTORY BECOMING A JAPANESE ARTIST.
+                      <br/>
+                      GEISHA WANT TO FIND A NEW IDENTITY THAT HAD BEEN LOST IN THE MODERN ERA BECAUSE OF DOUBT AND MISCONCEPTION.
+                      <br/>
+                      AFTER A LONG JOURNEY, THE GEISHA FINALLY FOUND A WAY TO ENTER THE SOLANA BLOCKCHAIN AND WAS READY TO MAKE A NEW LIFE THAT WILL BE ETERNAL.
+                    </p>
+                    {/* <Home
+                      candyMachineId={candyMachineId}
+                      config={config}
+                      connection={connection}
+                      startDate={startDateSeed}
+                      treasury={treasury}
+                      txTimeout={txTimeout}
+                    /> */}
+                  </div>
+                </div>
+              </section>
+              <section id="theclan" className="Featured_Wrapper__FXeTj">
+                <div className="Container Featured_Container__21lZR">
+                  <div className="Featured_Heading__1R2vu">
+                    <img
+                      src="./img-teardrop.svg"
+                      alt=""
+                      className="Featured_Teardrop__8QAEJ"
+                    />
+                    <h3 className="Featured_TheClan__24vL9">
+                      THE CLAN
+                    </h3>
+                  </div>
+                </div>
+                <div className="carousel Featured_Carousel__iDGrD">                  
+                </div>
+                <AliceCarousel mouseTracking infinite={true} autoPlay={true} autoHeight={false} autoWidth={true} items={items} />
+              </section>
+
+
+              {/* <section className="section main-section">
+                <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
+                  <div className="w-full md:w-3/5 justify-center text-center">
+                    <img
+                      className="md:w-5/5 z-50"
+                      width="550px"
+                      alt="NFT name"
+                      src="/logo.png"
+                    />
+                  </div>
+                  <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+                    <p className="title text-2xl font-bold">
+                      This will be <span className="font-bold">{nftName}</span>{" "}
+                      headline
+                    </p>
+                    <p className="mt-3">
+                      This will be {nftName} short description
+                    </p>
+                    <Home
+                      candyMachineId={candyMachineId}
+                      config={config}
+                      connection={connection}
+                      startDate={startDateSeed}
+                      treasury={treasury}
+                      txTimeout={txTimeout}
+                    />
+                  </div>
+                </div>
+              </section> */}
+            </div>
+          </WalletDialogProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </ThemeProvider>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,19 @@
 import "./Header.css";
 import { FaDiscord } from 'react-icons/fa';
-// import { Button } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 // import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className='flex flex-row justify-around'>
-            {/* <NavLink exact to='/'>
+            <a href='#home'>
                 <Button variant="text">Home</Button>
-            </NavLink>
-            <NavLink exact to='/roadmap'>
-                <Button variant="text">Roadmap</Button>
-            </NavLink>
-            <NavLink exact to='/faq'>
-                <Button variant="text">FAQ</Button>
-            </NavLink> */}
-            
-            {/** Change your discord link here */}
-            <a href='https://discord.gg/hK9jDZHe' rel="noreferrer" target='_blank'>
-                <FaDiscord size={22} color='#ffffff'/>
+            </a>
+            <a href='#journey'>
+                <Button variant="text">Journey</Button>
+            </a>
+            <a href='#theclan'>
+                <Button variant="text">Clan</Button>
             </a>
         </header>
     );
