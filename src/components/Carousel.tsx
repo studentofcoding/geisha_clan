ef248ebe1c5b7531d8570c297de5eabeeecc81ea
Yonathan Evan C <yonathanevan@gmail.com>
2021-11-27 09:29:40 +0700

    Update Content

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,19 +6,31 @@ const Carousel = () => {
 
       <div className="carousel h-1/2 carousel-center rounded-box">
         <div className="carousel-item">
-          <img style={{height: "100%"}} alt="gallery" src="./1.jpeg" className="rounded-box" />
+          <img style={{height: "100%"}} alt="gallery" src="./001.png" className="rounded-box" />
         </div> 
         <div className="carousel-item">
-          <img style={{height: "100%"}} alt="gallery" src="./2.jpeg" className="rounded-box" />
+          <img style={{height: "100%"}} alt="gallery" src="./002.png" className="rounded-box" />
         </div> 
         <div className="carousel-item">
-          <img style={{height: "100%"}} alt="gallery" src="./3.jpeg" className="rounded-box" />
+          <img style={{height: "100%"}} alt="gallery" src="./003.png" className="rounded-box" />
         </div> 
         <div className="carousel-item">
-          <img style={{height: "100%"}} alt="gallery" src="./4.jpeg" className="rounded-box" />
+          <img style={{height: "100%"}} alt="gallery" src="./004.png" className="rounded-box" />
         </div> 
         <div className="carousel-item">
-          <img style={{height: "100%"}} alt="gallery" src="./5.jpeg" className="rounded-box" />
+          <img style={{height: "100%"}} alt="gallery" src="./005.png" className="rounded-box" />
+        </div>
+        <div className="carousel-item">
+          <img style={{height: "100%"}} alt="gallery" src="./006.png" className="rounded-box" />
+        </div> 
+        <div className="carousel-item">
+          <img style={{height: "100%"}} alt="gallery" src="./007.png" className="rounded-box" />
+        </div> 
+        <div className="carousel-item">
+          <img style={{height: "100%"}} alt="gallery" src="./008.png" className="rounded-box" />
+        </div> 
+        <div className="carousel-item">
+          <img style={{height: "100%"}} alt="gallery" src="./009.png" className="rounded-box" />
         </div>
       </div>
       <p className="content text-center mt-3">
